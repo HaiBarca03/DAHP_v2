ff520970d00884b3f68e33f1d26395ffac9938e5
HaiBarca03 <doanduchai8d2m@gmail.com>
2024-05-05 09:44:38 +0700

    first commit

---
+++ src/app/home/home.js
@@ -0,0 +1,51 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);  
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+// chat box
+function ChatFunction() {
+    const element = document.querySelector('.chat__box');
+    element.classList.add('active__chat-box')
+    const element1 = document.querySelector('.fix__img');
+    element1.classList.add('active__fix-img')
+}
+function closeChat() {
+    const element = document.querySelector('.chat__box')
+    element.classList.remove('active__chat-box')
+    const element1 = document.querySelector('.fix__img');
+    element1.classList.remove('active__fix-img')
+}
